1c131fd1af804e8291476ed72cc55591e23922f4
Mtorres86 <iata24@outlook.es>
2023-06-12 13:06:13 +0200

    Se corrio el testUnit y funciono bien

--- src/main/java/hn/uht/torres/aplicacion_simple_maven_consola/App.java
+++ src/main/java/hn/uht/torres/aplicacion_simple_maven_consola/App.java
@@ -8,13 +8,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola desde maven consola" );
+        System.out.println("Calculadora con Maven" );
     }
 
+    //metodo lo crea desde el app test, podemos cambiar los valores de las variables y 
+   // solo cambiamos los valores que deseamos que se retornen
+	public static double sumar(double V1, double V2) {
+		// TODO Auto-generated method stub
+		return V1+V2;
+	}
+
+	public static double restar(double V1, double V2) {
+		// TODO Auto-generated method stub
+		return V1-V2;
+	}
+	
+	
 	
 
-	public static Object sumar(double d, double e) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 }
--- src/test/java/hn/uht/torres/aplicacion_simple_maven_consola/AppTest.java
+++ src/test/java/hn/uht/torres/aplicacion_simple_maven_consola/AppTest.java
@@ -1,34 +1,95 @@
 package hn.uht.torres.aplicacion_simple_maven_consola;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+
+
+
 
 /**
  * Unit test for simple App.
  */
+
+/**
+ * en esta etapa se ejecita la metodolia tdt que primero creo el codigo en version de prueba
+ * lo ejecuto y luego lo renderizo si es necesario
+ * 
+ * si todo marcha bien se procede a crear el codigo en el main*/
+
+//los test se hacen para probar que funcionar en todos los casos. 
+
+/**SE REFACTIORIZA UNA VEZX QUE SE EJECUTEN LAS PRUEBAS Y TODO ESTE CORRECTO*/
 public class AppTest {
 	
 	@Before
 	public void configuracionInicial()
 	{
-		System.out.println("se ejecuta al inicio de la pruebas");
+		System.out.println("Metodo tipo setup, se ejecuta antes de cada prueba");
 		
 	}	
+	
+	//aqui se evaluan las operaciones de la calculdora
 	@Test
 	public void testsuma() {
-		
+		//assertEquals se debe de espeficiar los margenes de error enm las sumas, en la operacion, o sea con que tanta exactitud se debe de hacer la oepracion
+		//este es mas exacto
+		//aqui le decimos que despues del asser mandamos a llamar nuestro metodo y le decimo que 
+				//5+6 son 11 y debera mostrar un error minino de 00001
 		assertEquals(App.sumar(5.0,6.0), 11,0, .00001);
 	}
 	
-	private void assertEquals(Object sumar, int expected, int actual, double delta) {
+	
+	
+
+	private void assertEquals(double sumar, int i, int j, double d) {
 		// TODO Auto-generated method stub
 		
 	}
-	@After
-	public void finalizacionPruebas()
+
+	@Test //esta es una anotacion para indicar que se hara una prueba
+	public void testsuma2() {
+		//assertTrue es mas general no se ocupa espeficiar el margen de error al ejeructar la operacion
+		//aqui le decimos que despues del asser mandamos a llamar nuestro metodo y le decimo que 
+		//8+7 son 15 y debera mostrar un error minino de 00001
+		
+		assertTrue(App.sumar(5.0,6.0), 11,0, .00001);
+	}
+	
+	
+	private void assertTrue(double sumar, int i, int j, double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Test
+	public void TestResta1() {
+		//aqui le decimos que despues del asser mandamos a llamar nuestro metodo y le decimo que 
+				//1-1 son 0 no necesita identificr el margend e error
+		//se recomienda trabajar con decimales
+		assertTrue(App.restar(1.0,1.0)== 0.0);
+		
+	}
+	private void assertTrue(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Test
+	public void TestResta2(){
+		assertTrue(App.restar(2.0,1.0)== 1.0);
+	}
+	
+	@Test
+	public void TestResta3(){
+		assertTrue(App.restar(1.0,2.0)== -1.0);
+	}
+	
+	@AfterClass
+	public static void finalizacionPruebas()
 	{
 		System.out.println(" tearDown se ejecuta despues de cada prueba");
 	}	
