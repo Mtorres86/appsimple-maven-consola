c2a2382506f301b18ce67c3953e5ad3aabf98fe9
Mtorres86 <iata24@outlook.es>
2023-06-12 16:55:22 +0200

    aplicacion terminada

--- src/main/java/hn/uht/torres/aplicacion_simple_maven_consola/App.java
+++ src/main/java/hn/uht/torres/aplicacion_simple_maven_consola/App.java
@@ -8,23 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Calculadora con Maven" );
+        System.out.println( "Hola desde maven consola" );
     }
 
-    //metodo lo crea desde el app test, podemos cambiar los valores de las variables y 
-   // solo cambiamos los valores que deseamos que se retornen
-	public static double sumar(double V1, double V2) {
-		// TODO Auto-generated method stub
-		return V1+V2;
-	}
-
-	public static double restar(double V1, double V2) {
-		// TODO Auto-generated method stub
-		return V1-V2;
-	}
-	
-	
 	
 
-	
+	public static Object sumar(double d, double e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
